52288fc021e3d674925620d20aa1378ea987921d
ianyuen <thanhianyuen@gmail.com>
2014-11-14 18:58:20 +0700

    - update environment

--- setEnv.bat
+++ setEnv.bat
@@ -1,5 +1,5 @@
 @echo off
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_71
+set JAVA_HOME=D:\DevTools\jdk1.7.0_71\
 set APACHE_ANT=D:\DevTools\apache-ant-1.9.4\bin
 set ANDROID_HOME=D:\DevTools\android-sdk-windows
 set ANDROID_TOOL=%ANDROID_HOME%\tools
