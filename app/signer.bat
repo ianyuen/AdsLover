48d7a9781318d7fc904e9994e18cee8c117776a3
ianyuen <thanhianyuen@gmail.com>
2014-11-20 20:26:38 +0700

    - update for build release

--- app/signer.bat
+++
@@ -1,3 +0,0 @@
-%JAVA_HOME%\bin\jarsigner -keystore iansoft.keystore -storepass P@ssw0rd %APP_NAME%-release-unsigned.apk ianyuen
-%ANDROID_HOME%\build-tools\21.0.2\zipalign -v 4 %APP_NAME%-release-unsigned.apk %APP_NAME%-release.apk
-%JAVA_HOME%\bin\jarsigner -verify %APP_NAME%-release.apk
--- make_debug.bat
+++ make_debug.bat
@@ -1,5 +1,4 @@
 call config.bat
 cd app
 call make_apk.bat release
-call signer.bat
 cd ..
